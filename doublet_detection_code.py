d4798baf5a010142bb87b091dea8aa35f164634a
Guang Yang <gyangbio@sh03-10n47.int>
2025-07-15 18:15:54 -0700

    Delete one tmp tutorial

--- doublet_detection_code.py
+++
@@ -1,5 +0,0 @@
-raw_matrix = dat.X.todense()
-scrub = scr.Scrublet(raw_matrix)
-doublet_score, predicted_doublets = scrub.scrub_doublets()
-real_cells = np.logical_not(predicted_doublets)
-dat = dat[real_cells,:]
